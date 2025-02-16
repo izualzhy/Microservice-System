e747a7224b00e4efa598eb15d9fea93508bd9f49
izualzhy <izualzhy@163.com>
2025-02-16 20:26:07 +0800

    feat(gateway): add support for HTTP and WebSocket routing
    - Implement separate routes for WebSocket and HTTP requests
    - Add log4j2 configuration for improved logging
    - Create Maven project configuration for Spring Cloud Gateway

--- gateway/src/main/java/cn/izualzhy/gateway/config/FilterConfig.java
+++ gateway/src/main/java/cn/izualzhy/gateway/config/FilterConfig.java
@@ -21,8 +21,16 @@ public class FilterConfig {
                         .filters(f -> f.filter(dynamicRouteFilter))
                         .uri("http://default-service-2")) // 这里的 uri 只是占位，实际由过滤器修改
                 .route("websocket_route", r -> r
-                        .path("/ws/**")  // 仅匹配 WebSocket 请求
+                        .path("/ws/**")
+                        .and()
+                        .header("Upgrade", "websocket") // 只处理 WebSocket 请求// 仅匹配 WebSocket 请求
                         .uri("ws://localhost:8201")) // 转发到 WebSocket 服务器
+                // 普通 HTTP 连接（http://）
+                .route("http_route", r -> r
+                        .path("/ws/**")  // 仍然匹配 /ws/**，但仅限于 HTTP
+                        .and()
+                        .not(r1 -> r1.header("Upgrade", "websocket")) // 过滤掉 WebSocket 请求
+                        .uri("http://localhost:6001")) // 转发到 HTTP 服务器
                 .build();
     }
 
